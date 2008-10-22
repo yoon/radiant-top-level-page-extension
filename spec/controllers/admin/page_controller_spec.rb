36495a32279004f9ca776a1ecdce95b6bbc15960
Mark Yoon <yoon@northwestern.edu>
2008-10-22 16:53:48 -0500

    added basic specs for page controller

--- spec/controllers/admin/page_controller_spec.rb
+++ spec/controllers/admin/page_controller_spec.rb
@@ -1,27 +1,32 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe Admin::PageController do
+  scenario :top_level_page_users, :pages
+
+  integrate_views
+
+  before :each do
+    login_as :top_level_set
+  end
 
   it "should allow you to view the index" do
-    pending
     get :index
     response.should be_success
     assigns(:homepage).should be_kind_of(Page)
+    assigns(:homepage).title.should == "Child"
   end
 
   it "should allow the index to render even with there are no pages" do
-    pending
     Page.destroy_all
     get :index
     response.should be_success
     assigns(:homepage).should be_nil
   end
 
-  it "should show the tree partialy expanded on the index" do
-    pending
+  it "should show the tree with Child, but not Home" do
     get :index
     response.should be_success
-    assert_rendered_nodes_where { |page| [nil, page_id(:home)].include?(page.parent_id) }
+    response.should have_tag("tr#page-#{page_id(:child)}")
+    response.should_not have_tag("tr#page-#{page_id(:home)}")
   end
-
 end
